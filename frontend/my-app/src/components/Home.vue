f07e5b68c1cf1526ae71f173a2cf74101cd8acfa
Juan Diego López <juandlopeztriana@gmail.com>
2018-11-16 19:06:27 -0500

    Vuex added

--- frontend/my-app/src/components/Home.vue
+++ frontend/my-app/src/components/Home.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Home</h1>
+  <h1>{{title}}</h1>
 </template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  computed: {
+    ...mapState(['title'])
+  }
+}
+</script>
